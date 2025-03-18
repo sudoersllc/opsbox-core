a10248cff895bd7390b4440471493845d4d3d3d4
Tyler Connelly <tconnelly@gsudoers.com>
2025-03-18 07:56:07 -0400

    Add try-except to cli_output

--- opsbox/bundled/cli/cli/cli.py
+++ opsbox/bundled/cli/cli/cli.py
@@ -21,10 +21,16 @@ class CLIOutput:
         Args:
             results (list[FormattedResult]): The formatted results from the checks.
         """
-        console.rule("CLI output")
-        resultant_plugins = [item.result_name for item in results]
-        console.print(f"\n[bold green]You have {len(results)} results from the following plugins:[/bold green] [red]{resultant_plugins}[/red]\n")
-        for result in results:
-            console.rule(f"[bold cyan]{result.result_name}[/bold cyan]")
-            console.print(Pretty(result.details))
-            console.rule()
+        try:
+            console.rule("CLI output")
+            resultant_plugins = [item.result_name for item in results]
+            console.print(f"\n[bold green]You have {len(results)} results from the following plugins:[/bold green] [red]{resultant_plugins}[/red]\n")
+            for result in results:
+                console.rule(f"[bold cyan]{result.result_name}[/bold cyan]")
+                console.print(Pretty(result.details))
+                console.rule()
+        except Exception:
+            logger.warning("Failed to print pretty. Printing raw results instead.")
+            for result in results:
+                logger.info(f"Result from {result.result_name}: {result.details}")
+            logger.info("Finished processing results")
